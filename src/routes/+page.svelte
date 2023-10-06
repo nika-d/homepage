33b1df5d37a35333848b8241f376a8db8d779842
nika <monika.domanska@charite.de>
2023-10-06 08:26:48 +0200

    todos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
 <h1>Welcome</h1>
 <p><a data-sveltekit-replacestate href="/HexagonSvgPattern">HexagonSvgPattern</a> - pattern of icons in hexagon layout with some colors</p>
+<!--
+@todo hexagon aus homepage raus
+@todo homepage feature beschreibung im readme
+@todo hexagon in ein projekt mit lisa graphs
+-->
